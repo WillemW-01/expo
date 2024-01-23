5e85ff5050feb23203df677943e6df02d45f37a8
WillemW-01 <daeben72@gmail.com>
2024-01-23 15:43:29 +0200

    Made login/register page with working fetch function

--- gym-test/frontend-vite/src/App.jsx
+++ gym-test/frontend-vite/src/App.jsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChange = (event, func) => {
+    func(event.target.value);
+  };
+
+  const login = () => {
+    let userData = JSON.stringify({
+      username: username,
+      password: password,
+      email: email,
+    });
+
+    console.log(userData);
+
+    fetch("http://127.0.0.1:5173/gym/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: userData,
+    })
+      .then((res) => res.json()) // get the response as JSON
+      .then((data) => {
+        console.log(data); // the data from the Django server
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const register = () => {
+    let userData = JSON.stringify({
+      username: username,
+      password: password,
+      email: email,
+    });
+
+    console.log(userData);
+
+    fetch("http://127.0.0.1:5173/gym/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: userData,
+    })
+      .then((res) => res.json()) // get the response as JSON
+      .then((data) => {
+        console.log(data); // the data from the Django server
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <fieldset>
+          <legend>Username</legend>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => handleChange(e, setUsername)}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Password</legend>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => handleChange(e, setPassword)}
+          />
+        </fieldset>
+        <button type="submit">Login</button>
+      </form>
+
+      <h1>Register</h1>
+      <form onSubmit={register}>
+        <fieldset>
+          <legend>Username</legend>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => handleChange(e, setUsername)}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Email</legend>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => handleChange(e, setEmail)}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Password</legend>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => handleChange(e, setPassword)}
+          />
+        </fieldset>
+        <button type="submit">Register</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
