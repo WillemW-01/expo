3fad9300c8d45f2cc439ed1a5ddef2c0c3a273e2
WillemW-01 <daeben72@gmail.com>
2024-02-07 12:21:57 +0200

    Updated flask backend to successfully create and login users, also db updated

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,6 +1,23 @@
+import json
 import os
 
-from flask import Flask
+from flask import Flask, abort, make_response, request
+from flask_cors import CORS
+from flaskr.db import get_db
+
+
+def request_data_valid(request: dict[str], keys):
+    try:
+        for key in keys:
+            print(f"Checking key {key}")
+            if key not in request.keys() or (
+                key in request.keys() and request[key] == ""
+            ):
+                return False
+        return True
+    except:
+        print("Exception")
+        return False
 
 
 def create_app(test_config=None):
@@ -9,6 +26,8 @@ def create_app(test_config=None):
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "../django/gymdb.sqlite"),
     )
+
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
@@ -20,12 +39,47 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/gym/login")
+    @app.route("/gym/login", methods=["POST", "GET"])
     def login():
-        return "Trying to login"
+        try:
+            db = get_db()
+            data = json.loads(request.data.decode("utf-8"))
+            if request_data_valid(data, ["username", "password"]):
+                users = db.execute("SELECT * FROM gym_user").fetchall()
+                for user in users:
+                    if user["username"] == data["username"]:
+                        db.close()
+                        return make_response("Success!", 200)
+                else:
+                    db.close()
+                    return make_response("User does not exist", 409)
+            else:
+                db.close()
+                return make_response("Invalid data", 400)
+        except Exception as e:
+            return f"Error: {e}"
 
-    @app.route("/")
-    def home():
-        return ""
+    @app.route("/gym/register", methods=["GET", "POST"])
+    def register():
+        db = get_db()
+        data = json.loads(request.data.decode("utf-8"))
+        if request_data_valid(data, ["username", "email", "password"]):
+            try:
+                db.execute(
+                    "INSERT INTO gym_user (username, email, password) values (?, ?, ?)",
+                    (data["username"], data["email"], data["password"]),
+                )
+                db.commit()
+                db.close()
+            except Exception as e:
+                print(e)
+                db.close()
+                return make_response("Username already taken.", 409)
+        else:
+            db.close()
+            return make_response("Data invalid.", 400)
+
+        db.close()
+        return make_response("Success!", 200)
 
     return app
