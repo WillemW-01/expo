f2462bd067b9aab008d96c353d5a12778ddf8d5f
WillemW-01 <daeben72@gmail.com>
2024-02-07 12:24:36 +0200

    Moved db to outside django folder and updated paths

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -24,7 +24,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "../django/gymdb.sqlite"),
+        DATABASE=os.path.join(app.instance_path, "../gymdb.sqlite"),
     )
 
     CORS(app)
